f87e308771266169624d85479af84b244aa60d64
Jason Klaassen <jwk24@students.calvin.edu>
2019-10-03 11:46:54 -0400

    Add Layer2Display and L2Frame

---
+++ L2Frame.java
@@ -0,0 +1,76 @@
+public class L2Frame {
+
+    private Integer dest, source, type, vlanid, checksum, payloadSize;
+    private String payload;
+
+    public L2Frame(Integer dest, Integer source, Integer type, Integer vlandid, String payload) {
+        this.dest = dest;
+        this.source = source;
+        this.type = type;
+        this.vlanid = vlandid;
+        this.payload = payload;
+        this.payloadSize = payload.length();
+    }
+
+    public static String toBinary(Integer number, Integer length) {
+        return "";
+    }
+
+    public Integer getDest() {
+        return dest;
+    }
+
+    public void setDest(Integer dest) {
+        this.dest = dest;
+    }
+
+    public Integer getSource() {
+        return source;
+    }
+
+    public void setSource(Integer source) {
+        this.source = source;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public Integer getVlanid() {
+        return vlanid;
+    }
+
+    public void setVlanid(Integer vlanid) {
+        this.vlanid = vlanid;
+    }
+
+    public Integer getChecksum() {
+        return checksum;
+    }
+
+    public void setChecksum(Integer checksum) {
+        this.checksum = checksum;
+    }
+
+    public Integer getPayloadSize() {
+        return payloadSize;
+    }
+
+    public void setPayloadSize(Integer payloadSize) {
+        this.payloadSize = payloadSize;
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+    
+
+}
--- TestServer.java
+++ TestServer.java
@@ -5,8 +5,31 @@
  */
 public class TestServer {
 	public static void main(String args[]) {
+		runTests();
 		LightSystem system = new LightSystem();
 		LightDisplay d1 = new LightDisplay(new LightPanel());
 		BitDisplay bd = new BitDisplay(new BitHandler());
+	}
+
+	private static void runTests() {
+		testL2Frame();
+		System.out.println("L2Frame Passed.");
+	}
+
+	private void testL2Frame() {
+		testToBinary();
+		L2Frame frame = new L2Frame(7, 3, 0, 0, "010101010101");
+	}
+
+	private void testToBinary() {
+		Integer five = 5, seven = 7, fifteen = 15;
+		Integer length = 4;
+		String bitString = L2Frame.toBinary(five, length);
+		if(bitString != "0101") System.err.println("Test Failed: L2Frame.toBinary(): Integer 5 != '0101'");
+		bitString = L2Frame.toBinary(seven, length);
+		if(bitString != "0111") System.err.println("Test Failed: L2Frame.toBinary(): Integer 5 != '0111'");
+		bitString = L2Frame.toBinary(fifteen, length);
+		if(bitString != "1110") System.err.println("Test Failed: L2Frame.toBinary(): Integer 5 != '1110'");
+		System.out.println("All Tests Passed for toBinary().");
 	}
 }
